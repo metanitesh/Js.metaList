7c7fad0715b96091bce72293e6a4f2b261e98ca7
nitesh sharma <nitesh.grep@gmail.com>
2014-06-09 14:36:41 +0530

    refactoring mock data file

--- js/mockData.js
+++ js/mockData.js
@@ -1,4 +1,3 @@
-/*1*/
 var data = [{
 	title: "bucketList",
 	id: 1,
@@ -27,37 +26,4 @@ var data = [{
 		done: false,
 		comments: ["whitewalkers seems to be in no hurry"]
 	}]
-}]
-
-/*2*/
-
-// [{
-// 	collection - title: "bucketList",
-// 	collection - id: 1,
-// 	title: "play soccer for world league",
-// 	id: 1,
-// 	done: false,
-// 	comments: ["fifa 2014 is about to start", "need to go buy a Brazil T-shirt"]
-// }, {
-// 	collection - title: "bucketList",
-// 	collection - id: 1,
-// 	title: "start a school",
-// 	id: 2,
-// 	done: true,
-// 	comments: ["start with being a mentor"]
-// }, {
-// 	collection - title: "to-do",
-// 	collection - id: 2,
-// 	title: "create a todo App",
-// 	id: 1,
-// 	done: false,
-// 	comments: []
-// }, {
-// 	collection - title: "to-do",
-// 	collection - id: 2,
-// 	title: "watch GOT",
-// 	id: 2,
-// 	done: false,
-// 	comments: ["whitewalkers seems to be in no hurry"]
-
-// }]
+}];
