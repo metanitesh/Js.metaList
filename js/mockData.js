18b16d93b51eb5dd541c5172d2fdf6fcca02ed3a
nitesh sharma <nitesh.grep@gmail.com>
2014-06-09 14:35:58 +0530

    adding mock data file

---
+++ js/mockData.js
@@ -0,0 +1,63 @@
+/*1*/
+var data = [{
+	title: "bucketList",
+	id: 1,
+	tasks: [{
+		title: "play soccer for world league",
+		id: 1,
+		done: false,
+		comments: ["fifa 2014 is about to start", "need to go buy a Brazil T-shirt"]
+	}, {
+		title: "start a school",
+		id: 2,
+		done: true,
+		comments: ["start with being a mentor"]
+	}]
+}, {
+	title: "to-do",
+	id: 2,
+	tasks: [{
+		title: "create a todo App",
+		id: 1,
+		done: false,
+		comments: []
+	}, {
+		title: "watch GOT",
+		id: 2,
+		done: false,
+		comments: ["whitewalkers seems to be in no hurry"]
+	}]
+}]
+
+/*2*/
+
+// [{
+// 	collection - title: "bucketList",
+// 	collection - id: 1,
+// 	title: "play soccer for world league",
+// 	id: 1,
+// 	done: false,
+// 	comments: ["fifa 2014 is about to start", "need to go buy a Brazil T-shirt"]
+// }, {
+// 	collection - title: "bucketList",
+// 	collection - id: 1,
+// 	title: "start a school",
+// 	id: 2,
+// 	done: true,
+// 	comments: ["start with being a mentor"]
+// }, {
+// 	collection - title: "to-do",
+// 	collection - id: 2,
+// 	title: "create a todo App",
+// 	id: 1,
+// 	done: false,
+// 	comments: []
+// }, {
+// 	collection - title: "to-do",
+// 	collection - id: 2,
+// 	title: "watch GOT",
+// 	id: 2,
+// 	done: false,
+// 	comments: ["whitewalkers seems to be in no hurry"]
+
+// }]
