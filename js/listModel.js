abacddcdd40775aa87a4d98939f96fa4e40236bd
nitesh sharma <nitesh.grep@gmail.com>
2014-06-10 12:21:08 +0530

    implementing add, generateID feature

--- js/listModel.js
+++ js/listModel.js
@@ -1,14 +1,31 @@
-var List = function(){
+var List = function() {
+	this.init.apply(this, arguments)
 
 };
 
 List.records = [];
 
+
 List.prototype = {
-	add: function(obj){
-		List.records.push(obj)
+	
+	init: function() {
+		var recordAtrributes = arguments[0];
+		if(!recordAtrributes) throw ("no attribute for record");
+		if(!this.id) this.id = this.genrateId();	
+		this.add(recordAtrributes);
 	},
 
-	
-	
+	add: function(recordAtrributes) {
+		$.extend(true, this, recordAtrributes);
+
+	},
+
+	genrateId: function() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random() * 16 | 0,
+				v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		}).toUpperCase();
+	}
+
 }
