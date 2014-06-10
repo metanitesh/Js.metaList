2da1b9d3f8cd26fc25a627873a8fe83d398bc61b
nitesh sharma <nitesh.grep@gmail.com>
2014-06-10 13:02:15 +0530

    implementing save and delete feature

--- js/listModel.js
+++ js/listModel.js
@@ -9,15 +9,16 @@ List.records = [];
 List.prototype = {
 	
 	init: function() {
+		this.constuctor = List
 		var recordAtrributes = arguments[0];
-		if(!recordAtrributes) throw ("no attribute for record");
+		if(!recordAtrributes) throw ("at least required title attribute");
 		if(!this.id) this.id = this.genrateId();	
 		this.add(recordAtrributes);
 	},
 
 	add: function(recordAtrributes) {
 		$.extend(true, this, recordAtrributes);
-
+		return this;
 	},
 
 	genrateId: function() {
@@ -26,6 +27,22 @@ List.prototype = {
 				v = c == 'x' ? r : (r & 0x3 | 0x8);
 			return v.toString(16);
 		}).toUpperCase();
-	}
+	},
 
+	save: function(){
+		this.constuctor.records.push(this);
+		return this;
+	},
+
+	delete: function(){
+		var records = this.constuctor.records;
+		for(var i=0, max=records.length; i<max; i++){
+			if( this.id === records[i].id){
+				records.pop(records[i]);
+				return records[i];
+			}
+		}
+	}
+	
+	
 }
