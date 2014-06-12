046508f75fc525c0d540048d66a5185fe5f8bf4d
nitesh sharma <nitesh.grep@gmail.com>
2014-06-12 18:14:55 +0530

    adding populate list spec

--- tests/spec/listModelSpec.js
+++ tests/spec/listModelSpec.js
@@ -32,69 +32,7 @@ define(["ListModel"], function(ListModel) {
 		});
 
 		it("should create object in records collection", function() {
-			
-			/** given **/
-			var careerList = new ListModel({
-				id: "123123-12",
-				title: "careers"
-			});
 
-			/** when **/
-			careerList.create();
-			
-			/** then **/
-			expect(ListModel.records["123123-12"].title).toEqual("careers");
-
-		});
-
-		it("should update object in records collection", function() {
-
-			/** given **/
-			var careerList = new ListModel({
-				id: "123123-12",
-				title: "careers"
-			});
-			careerList.create();
-
-			/** when **/
-			careerList.tasks = ["chef", "craftsman"];
-			careerList.update()
-
-			/** then **/
-			expect(ListModel.records["123123-12"].tasks).toEqual(["chef", "craftsman"]);
-
-		});
-
-		it("should create if object is new or update if object is old", function(){
-			/** given **/
-			var careerList = new ListModel({
-				id: "123123-12",
-				title: "careers"
-			});
-			
-			/** when **/
-			careerList.save();	
-			careerList.tasks = ["chef", "craftsman"];
-			careerList.save()
-
-			/** then **/
-			expect(ListModel.records["123123-12"].title).toEqual("careers");
-			expect(ListModel.records["123123-12"].tasks).toEqual(["chef", "craftsman"]);
-		})
-
-		it("should return clone of the object", function(){
-			
-			/** when **/
-			var clone = travelList.clone();
-			
-			/** then **/
-			expect(clone.title).toEqual(travelList.title);
-
-
-		});
-
-		it("should destroy object from records collection", function(){
-			
 			/** given **/
 			var careerList = new ListModel({
 				id: "123123-12",
@@ -103,7 +41,35 @@ define(["ListModel"], function(ListModel) {
 
 			/** when **/
 			careerList.save();
-			
+
+			/** then **/
+			expect(ListModel.records["123123-12"].title).toEqual("careers");
+
+		});
+
+
+		it("should return clone of the object", function() {
+
+			/** when **/
+			var clone = travelList.clone();
+
+			/** then **/
+			expect(clone.title).toEqual(travelList.title);
+
+
+		});
+
+		it("should destroy object from records collection", function() {
+
+			/** given **/
+			var careerList = new ListModel({
+				id: "123123-12",
+				title: "careers"
+			});
+
+			/** when **/
+			careerList.save();
+
 			/** then **/
 			expect(ListModel.records["123123-12"].title).toEqual("careers");
 
@@ -121,70 +87,85 @@ define(["ListModel"], function(ListModel) {
 				id: "123123-12",
 				title: "chef"
 			});
-			careerList.create();
+			careerList.save();
 
 			/** when **/
 			var record = ListModel.findById("123123-12");
-			
+
 			/** then **/
 			expect(record).toEqual(careerList);
 		})
 
-		it("should populate object from objects array", function(){
+		it("should populate object from objects array", function() {
 			/** given **/
 			var listCollcection = [{
 				title: "bucketList",
-				id: 1,
-				tasks: [{
-					title: "play soccer for world league",
-					id: 1,
-					done: false,
-					comments: ["fifa 2014 is about to start", "need to go buy a Brazil T-shirt"]
-				}, {
-					title: "start a school",
-					id: 2,
-					done: true,
-					comments: ["start with being a mentor"]
-				}]
+				id: 11111,
+				tasks: {
+					1001201: {
+						title: "play soccer for world league",
+						id: 1001201,
+						done: false,
+						comments: ["fifa 2014 is about to start", "need to go buy a Brazil T-shirt"]
+					},
+					2023402304: {
+						title: "start a school",
+						id: 2023402304,
+						done: true,
+						comments: ["start with being a mentor"]
+					}
+				}
 			}, {
 				title: "to-do",
-				id: 2,
-				tasks: [{
-					title: "create a todo App",
-					id: 1,
-					done: false,
-					comments: []
-				}, {
-					title: "watch GOT",
-					id: 2,
-					done: false,
-					comments: ["whitewalkers seems to be in no hurry"]
-				}]
+				id: 2222,
+				tasks: {
+					1234: {
+						title: "create a todo App",
+						id: 1234,
+						done: false,
+						comments: []
+					},
+					22434: {
+						title: "watch GOT",
+						id: 22434,
+						done: false,
+						comments: ["whitewalkers seems to be in no hurry"]
+					}
+				}
 			}];
 
 			/** when **/
 			ListModel.populate(listCollcection);
+			console.log(ListModel.records);
 
 			/** then **/
-			expect(ListModel.records[1].title).toEqual("bucketList");
-			expect(ListModel.records[2].title).toEqual("to-do");
+			expect(ListModel.records[11111].title).toEqual("bucketList");
+			expect(ListModel.records[2222].title).toEqual("to-do");
+			expect(ListModel.records[11111].tasks[1001201].title).toEqual("play soccer for world league");
+			expect(ListModel.records[2222].tasks[22434].title).toEqual("watch GOT");
 		});
 
-		it("should add Task to list's tasks object", function(){
-			
+		it("should add Task to list's tasks object", function() {
+
 			/** when **/
-			travelList.addTask({id: 123423, title: "bali"});
-			
+			travelList.addTask({
+				id: 123423,
+				title: "bali"
+			});
+
 			/** then **/
 			expect(travelList.tasks[123423].title).toEqual("bali");
 		});
 
-		it("should find task from list's tasks object", function(){
-			
+		it("should find task from list's tasks object", function() {
+
 			/** when **/
-			travelList.addTask({id: 123423, title: "bali"});
+			travelList.addTask({
+				id: 123423,
+				title: "bali"
+			});
 			var task = travelList.findTaskById(123423);
-			
+
 			/** then **/
 			expect(task.title).toEqual("bali");
 
