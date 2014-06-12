9c55b4298d718a3893102724f3d4bc991b6f36d2
nitesh sharma <nitesh.grep@gmail.com>
2014-06-12 12:43:40 +0530

    refactored ListModel

--- js/app/listModel.js
+++ js/app/listModel.js
@@ -1,24 +1,23 @@
 define(["Model", "TaskModel"], function(Model) {
 
-	var ListModel = new Model();
+	var ListModel = Model.create();
 
 	ListModel.extend({
 
-		records: [],
+		records: {},
 
-		populate: function(listCollaction) {
-			listCollaction.forEach(function(item) {
-				var model = new ListModel(item);
-				model.save();
-			});
-		},
-		findById: function(id) {
-			var records = this.records;
+		populate: function(records) {
 			for (var i = 0, max = records.length; i < max; i++) {
-				if (records[i].id == id) {
-					return records[i];
-				}
+				var record = new this(records[i]);
+				record.create();
 			}
+		},
+
+		findById: function(id) {
+			var record = this.records[id];
+			if (!record) throw "no record found";
+			return record.clone();
+
 		}
 
 	});
@@ -27,18 +26,20 @@ define(["Model", "TaskModel"], function(Model) {
 	ListModel.include({
 
 		init: function(attr) {
-			var recordAtrributes = attr;
-			if (!recordAtrributes) throw ("at least required title attribute");
-			this.add(recordAtrributes);
+
+			if (!attr) throw ("at least required title attribute");
+
+			this.load(attr);
 			if (!this.id) this.id = this.genrateId();
-			if (!this.tasks) this.tasks = [];
+			// if (!this.tasks) this.tasks = {};
 
 		},
 
-		add: function(recordAtrributes) {
-			$.extend(true, this, recordAtrributes);
+		load: function(attr) {
+			$.extend(true, this, attr);
 			return this;
 		},
+
 
 		genrateId: function() {
 			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -48,55 +49,45 @@ define(["Model", "TaskModel"], function(Model) {
 			}).toUpperCase();
 		},
 
-		iterator: function(callback) {
-			var records = this.parent.records;
-			for (var i = 0, max = records.length; i < max; i++) {
-				if (this.id === records[i].id) {
-					records.pop(records[i]);
-					records.push(this);
-					return records[i];
-				}
-			}
-
+		create: function() {
+			this.parent.records[this.id] = this.clone();
 		},
+
+		update: function() {
+			this.parent.records[this.id] = this.clone();
+		},
+
+
 		save: function() {
-			var records = this.parent.records;
-			for (var i = 0, max = records.length; i < max; i++) {
-				if (this.id === records[i].id) {
-					records.pop(records[i]);
-					records.push(this);
-					return records[i];
-				}
-			}
-
-			records.push(this);
-			return this;
-		},
-
-		delete: function() {
-			var records = this.parent.records;
-			for (var i = 0, max = records.length; i < max; i++) {
-				if (this.id === records[i].id) {
-					records.pop(records[i]);
-					return records[i];
-				}
+			if (this.parent.records[this.id]) {
+				this.update();
+			} else {
+				this.create();
 			}
 		},
 
-		addTask: function(attr) {
-
-			var task = new Task(attr);
-			task.parent = this;
-			task.save(this.id);
+		destroy: function() {
+			delete this.parent.records[this.id];
 		},
 
-		getTaskById: function(id) {
-			for (var i = 0; i < this.tasks.length; i++) {
-				if (id === this.tasks[i].id) {
-					return this.tasks[i];
-				}
-			}
+		clone: function() {
+			return $.extend(true, {}, this);
 		}
+
+		// addTask: function(attr) {
+
+		// 	var task = new Task(attr);
+		// 	task.parent = this;
+		// 	task.save(this.id);
+		// },
+
+		// getTaskById: function(id) {
+		// 	for (var i = 0; i < this.tasks.length; i++) {
+		// 		if (id === this.tasks[i].id) {
+		// 			return this.tasks[i];
+		// 		}
+		// 	}
+		// }
 	});
 
 	return ListModel;
