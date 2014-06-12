6564ab0e30b189d8b47d9b8cc347444d61dbf8c5
nitesh sharma <nitesh.grep@gmail.com>
2014-06-12 10:56:02 +0530

    adding inheritance support in Model class

--- js/app/Model.js
+++ js/app/Model.js
@@ -1,12 +1,21 @@
 define(["jquery"], function() {
 	var Model = function() {
+		
+	};
+
+	Model.create = function(parent){
 		var ModelClass = function() {
 			this.init.apply(this,arguments)
 		};
 
+		if(parent){
+			var F = function(){};
+			F.prototype = parent.prototype;
+			ModelClass.prototype = new F();
+		};
+
 		ModelClass.fn = ModelClass.prototype;
 		ModelClass.fn.parent = ModelClass;
-		ModelClass.name = "ModelYo"
 
 		ModelClass.extend = function(obj) {
 			$.extend(ModelClass, obj);
@@ -17,7 +26,6 @@ define(["jquery"], function() {
 		};
 
 		return ModelClass;
-	};
-
+	}
 	return Model;
 });
