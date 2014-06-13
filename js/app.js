671fa7d12c4c0de8a7222c45d8618d5337c7d5af
nitesh sharma <nitesh.grep@gmail.com>
2014-06-13 15:53:07 +0530

    adding test controller in app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-define(["ListModel"], function(ListModel){
-	window.list = new  ListModel();
-	window.List = ListModel;
-	
+define(["ListController", "ListModel",  "jquery"], function(ListController, ListModel, $){
+	// var life  = new	ListModel({title: "bucketlist"});
+	// life.addTask({title: "skydive", content: "yo baby", comments:["did it"]});
+	// life.addTask({title: "party", content: "on my way", comments:["have to do it"]});
+	// life.save();
+	// console.log(ListModel.records);
+	$(function(){
+		var listView = new ListController(".list-view", "#list-item");
+		
+		// $(".list-container").append(listView.addList({title: "some"}))
+		// $(".list-container").append(listView.addList({title: "more"}))
 
-})
+		console.log(ListModel.records);
+
+	})
+});
