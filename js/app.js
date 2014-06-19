91aaa00f250bd7079f0bd29f17987827e83b4dd4
nitesh sharma <nitesh.grep@gmail.com>
2014-06-19 11:34:34 +0530

    app.js refactoring

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-define(["ListController", "TaskController", "NoteController", "CommentController", "ListModel", "TaskModel", "jquery"], function(ListController, TaskController, NoteController, CommentController, ListModel, TaskModel, $){
+define(["ListController", "TaskController", "NoteController", "CommentController", "jquery"], function(ListController, TaskController, NoteController, CommentController, $) {
 
-	$(function(){
-
-		listView = new ListController("#list-view", "#list-item");
-		var taskView = new TaskController("#task-view", "#task-item");
+	$(function() {
+		var listController = new ListController("#list-view", "#list-item");
+		var taskController = new TaskController("#task-view", "#task-item");
 		var detailContentController = new NoteController("#note-view");
 		var commentController = new CommentController("#comment-view", "#comment-item");
-		// $(".list-container").append(listView.addList({title: "some"}))
-		// $(".list-container").append(listView.addList({title: "more"}))
-		// window.listView = listView;
-		// window.taskView = taskView;
 	});
 });
