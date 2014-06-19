1fe660354c090f34225b1b713560b7f66ab29be5
nitesh sharma <nitesh.grep@gmail.com>
2014-06-19 11:48:10 +0530

    restructuring folders

---
+++ require_config.js
@@ -0,0 +1,20 @@
+require.config({
+    baseUrl: './',
+    paths: {
+        jquery: 'js/lib/jquery-1.11.1',
+        underscore: 'js/lib/underscore',
+
+        Model: 'js/app/core/Model',
+        Controller: 'js/app/core/Controller',
+
+        ListModel: 'js/app/list/listmodel',
+        TaskModel: 'js/app/task/taskmodel',
+        
+        ListController: 'js/app/list/listcontroller',
+        TaskController: 'js/app/task/taskController',
+        NoteController: 'js/app/note/NoteController',
+        CommentController: 'js/app/comment/CommentController'
+    }
+});
+
+
