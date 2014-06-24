852db0a5f46623808e94b0fd81fb77556b621d9b
nitesh sharma <nitesh.grep@gmail.com>
2014-06-24 19:19:29 +0530

    refactoring comment controller

--- js/app/app.js
+++ js/app/app.js
@@ -1,9 +1,10 @@
 define(["ListController", "TaskController", "NoteController", "CommentController", "jquery"], function(ListController, TaskController, NoteController, CommentController, $) {
 
 	$(function() {
+		
 		var listController = new ListController("#list-view", "#list-item");
 		var taskController = new TaskController("#task-view", "#task-item");
-		var detailContentController = new NoteController("#note-view");
+		var noteController = new NoteController("#note-view");
 		var commentController = new CommentController("#comment-view", "#comment-item");
 	});
 
