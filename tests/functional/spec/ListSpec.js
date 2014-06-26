6dbefc3080d1fec5b433c43917d97ac3078f89cc
nitesh sharma <nitesh.grep@gmail.com>
2014-06-26 12:37:07 +0530

    adding List Controller functional tests

--- tests/functional/spec/ListSpec.js
+++ tests/functional/spec/ListSpec.js
@@ -1,24 +1,68 @@
-define(["func"], function(func) {
+define(["ListModel", "ListController", "func"], function(ListModel, ListController, func) {
 	F.speed = 100;
 
-	describe('TodoMVC', function() {
-		it('should create and complete todos', function() {
-			var newTodo = F('.add-new-list', 0);
-			console.log(newTodo)
-			newTodo.type('text [enter]');
-			
-			// F('.todo label:contains("FuncUnit")', 0).visible();
-			// F('.todo label:contains("is")', 0).visible();
-			// F('.todo label:contains("awesome")', 0).visible();
+	describe('List', function() {
 
-			// F('.toggle:not(:checked)', 0).click();
-			// F('.toggle:not(:checked)', 0).click();
-			// F('.toggle:not(:checked)', 0).click();
+		beforeEach(function() {
+			var src = document.getElementById('metaListFrame').src
+			document.getElementById('metaListFrame').src = src;
 
-			// F('#clear-completed', 0).click();
-			// F('.todo.completed', 0).missing();
+			var addList = F('.add-new-list', 0);
+			addList.type('movies [enter]');
+		})
+
+		it('can create a new List', function() {
+
+			/****** then *****/
+			F(".list .title:contains('movies')", 0).visible();
+
 		});
 
 
+
+		it("can edit a list item", function() {
+
+			var performTest = function() {
+				F(this).closest(".list").dblclick()
+				F(this).closest(".list").find(".input-wrapper").visible(function() {
+					F(this).find(".edit-list-input").type("Bucket-List [enter]");
+					F(this).invisible();
+					F(this).closest(".list").find(".title").visible();
+				});
+			}
+
+			F(".list .title:contains('movies')", 0).visible(performTest);
+
+		});
+
+		it("can delete a list item", function() {
+
+			var performTest = function() {
+				F(this).closest(".list").find(".delete-list").click()
+				F(this).closest(".list").invisible();
+			}
+
+			F(".list .title:contains('movies')", 0).visible(performTest);
+
+		});
+
+		it("should apply active class to selected list", function() {
+			var performTest = function() {
+				F(this).closest(".list").wait(function() {
+					return F(this).hasClass("list-active");
+				});
+			};
+
+			F(".list .title:contains('Books')", 0).click(performTest);
+		});
+
+		it("should show tasks for selected list", function() {
+			var performTest = function() {
+				F(".task-item .task-title:contains('The Art Spirit')", 0).visible();
+				F(".task-item", 0).size(5);
+			};
+
+			F(".list .title:contains('Books')", 0).click(performTest);
+		});
 	});
 });
