569282e74e1c1b79a94144912fa50114673e00fe
nitesh sharma <nitesh.grep@gmail.com>
2014-06-26 10:20:56 +0530

    rearranging unit test folder

---
+++ tests/unit/spec/listControllerSpec.js
@@ -0,0 +1,14 @@
+define(["ListController"], function(ListController) {
+
+	describe("List-controller", function() {
+		
+
+		it("should extend object with user's custom attribute", function() {
+
+						
+		});
+
+		
+	});
+
+});
