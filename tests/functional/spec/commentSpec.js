07565abf2dd45553e01d89dc83a185aa5b2f627d
nitesh sharma <nitesh.grep@gmail.com>
2014-06-26 19:01:37 +0530

    adding comment's functional spec

---
+++ tests/functional/spec/commentSpec.js
@@ -0,0 +1,63 @@
+define(["func"], function(func) {
+	F.speed = 200;
+
+	describe('Comment', function() {
+		
+		beforeEach(function() {
+			var addTask;
+			var addList;
+
+			var src = document.getElementById('metaListFrame').src;
+			document.getElementById('metaListFrame').src = src;
+
+
+			addList = F('.add-new-list', 0);
+			addList.type('movies [enter]');
+
+			addTask = F('.add-task', 0);
+			addTask.type("Into The Wild [enter]");
+			addTask.type("Prestige [enter]");
+			addTask.type("Peaceful warrior [enter]");
+
+
+		});
+
+		it("should be disabled if no task selected", function() {
+			var performTest = function() {
+				F(".add-comment", 0).hasClass("disabled");
+				F(".add-comment", 0).attr("disabled", "disabled");
+			};
+			F(".list .title:contains('Books')", 0).click(performTest);
+		});
+
+		it("should display comment for the task", function(){
+			
+			var performTest = function() {
+				F(".task-item .task-title:contains('The Alchemist')", 0).click();
+
+				F(".comment:contains('I loved this book.')",0).visible();
+				F(".comment:contains('they even have graphic novel for this')",0).visible();
+			};
+
+			F(".list .title:contains('Books')", 0).click(performTest);
+
+		});
+
+		it("can add comment to the selected task", function() {
+			var performTest = function() {
+				F(".add-comment", 0).type("rather than love, than money, than faith, than fame, than fairness... give me truth. [enter]");
+				F(".add-comment", 0).type("Jon Krakauer [enter]");
+
+				F(".comment:contains('rather than love, than money, than faith, than fame, than fairness... give me truth.')",0).visible();
+				F(".comment:contains('Jon Krakauer')",0).visible();
+			};
+			F(".task-item .task-title:contains('Into The Wild')", 0).visible().click(performTest);
+		});
+		
+
+		
+
+
+
+	});
+});
