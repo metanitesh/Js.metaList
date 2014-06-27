f19a44060220a1dd7b307f99037701e314e88845
nitesh sharma <nitesh.grep@gmail.com>
2014-06-27 11:23:48 +0530

    adding actionbar controller

---
+++ js/app/actionbar/actionbarController.js
@@ -0,0 +1,28 @@
+define(["ListModel", "Controller", "util"], function(ListModel, Controller, util) {
+	
+	var ActionbarController = util.extend(Controller, {
+	constructor: function(){
+			this.super.constructor.apply(this, arguments);
+		},
+
+		events: {
+			"click save": "save"
+		},
+
+		elements: {
+			"save" : ".save",
+			"frame": ".save-frame"
+		},
+
+		addAnimation: function(){
+			this.view.frame.fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100);
+		},
+		
+		save: function(){
+			ListModel.saveLocal(ListModel.localDb);
+			this.addAnimation();
+		}
+	});
+
+	return ActionbarController;
+});
