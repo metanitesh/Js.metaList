3332acf34fd7c172ccb82ae5ea5fcf2120ba44b6
nitesh sharma <nitesh.grep@gmail.com>
2014-06-27 19:42:08 +0530

    updating grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,12 @@ module.exports = function(grunt) {
 				template: require('grunt-template-jasmine-requirejs'),
 				templateOptions: {
 					requireConfig: {
-						baseUrl: '',
+						baseUrl: '/',
 						paths: {
 							jquery: 'js/lib/jquery-1.11.1',
 							underscore: 'js/lib/underscore',
 
-							util: 'js/app/core/Util',
+							util: 'js/app/core/util',
 							Model: 'js/app/core/Model',
 							Controller: 'js/app/core/Controller',
 							bootstrapData: "js/app/bootstrapData",
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
 							TaskModel: 'js/app/task/Taskmodel',
 
 							ListController: 'js/app/list/ListController',
-							TaskController: 'js/app/task/TaskController',
+							TaskController: 'js/app/task/taskController',
 							NoteController: 'js/app/note/NoteController',
 							CommentController: 'js/app/comment/CommentController',
         					actionbarController: 'js/app/actionbar/actionbarController'
