484a9e5cfda38e29b6c3cd8a640465403753a446
nitesh sharma <nitesh.grep@gmail.com>
2014-06-28 17:39:09 +0530

    updating grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		jasmine: {
-			src: ['js/app/list/*.js', 'js/app/task/*.js'],
+			src: ['ListModel', 'TaskModel'],
 			options: {
 				specs: 'tests/unit/spec/*.js',
 				template: require('grunt-template-jasmine-requirejs'),
@@ -15,32 +15,32 @@ module.exports = function(grunt) {
 							jquery: 'js/lib/jquery-1.11.1',
 							underscore: 'js/lib/underscore',
 
-							util: 'js/app/core/Util',
-							Model: 'js/app/core/Model',
-							Controller: 'js/app/core/Controller',
-							bootstrapData: "js/app/bootstrapData",
+							util: "js/app/core/util",
+							Model: 'js/app/core/model',
+							Controller: 'js/app/core/controller',
 
-							ListModel: 'js/app/list/listModel',
-							TaskModel: 'js/app/task/taskModel',
+							ListModel: 'js/app/list/listmodel',
+							TaskModel: 'js/app/task/taskmodel',
+							bootstrapData: "js/app/bootstrapdata",
 
-							ListController: 'js/app/list/ListController',
-							TaskController: 'js/app/task/taskController',
-							NoteController: 'js/app/note/NoteController',
-							CommentController: 'js/app/comment/CommentController',
-        					actionbarController: 'js/app/actionbar/actionbarController'
-							
+							ListController: 'js/app/list/listcontroller',
+							TaskController: 'js/app/task/taskcontroller',
+							NoteController: 'js/app/note/notecontroller',
+							CommentController: 'js/app/comment/commentcontroller',
+							actionbarController: 'js/app/actionbar/actionbarcontroller',
 						}
-					}
-				}
-			}
+
+
+
+						
+					},
+
+				},
+							}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	// grunt.loadNpmTasks('grunt-contrib-jshint');
-
-	// grunt.registerTask('test', ['jshint', 'jasmine']);
-
 	grunt.registerTask('default', ['jasmine']);
 
 };
