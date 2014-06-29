c21c15621b6d602955455b16441c8fc7234d2bd9
nitesh sharma <nitesh.grep@gmail.com>
2014-06-29 17:42:24 +0530

    refactoring actionbar controller

--- js/app/actionbar/actionbarcontroller.js
+++ js/app/actionbar/actionbarcontroller.js
@@ -1,31 +1,8 @@
 define(["ListModel", "Controller", "util"], function(ListModel, Controller, util) {
-	
+
 	var ActionbarController = util.extend(Controller, {
-	constructor: function(){
+		constructor: function() {
 			this.super.constructor.apply(this, arguments);
-			$(".list_menu").click(function(){
-				$("#sidebar").hide();
-				$("#main-content").hide();
-				$("#detail").hide();
-
-				$("#sidebar").fadeIn();
-
-			});
-			$(".task_menu").click(function(){
-				$("#sidebar").hide();
-				$("#main-content").hide();
-				$("#detail").hide();
-
-				$("#main-content").fadeIn();
-
-			});$(".detail_menu").click(function(){
-				$("#sidebar").hide();
-				$("#main-content").hide();
-				$("#detail").hide();
-
-				$("#detail").fadeIn();
-
-			});
 		},
 
 		events: {
@@ -33,15 +10,15 @@ define(["ListModel", "Controller", "util"], function(ListModel, Controller, util
 		},
 
 		elements: {
-			"save" : ".save",
+			"save": ".save",
 			"frame": ".save-frame"
 		},
 
-		addAnimation: function(){
+		addAnimation: function() {
 			this.view.frame.fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100);
 		},
-		
-		save: function(){
+
+		save: function() {
 			ListModel.saveLocal(ListModel.localDb);
 			this.addAnimation();
 		}
