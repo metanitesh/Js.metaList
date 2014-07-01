5fb3dbb6d6d3e907e95bd958d417d0e61cc6d36f
nitesh sharma <nitesh.grep@gmail.com>
2014-07-01 11:56:45 +0530

    adding actionbar functional tests

---
+++ tests/functional/spec/actionbarSpec.js
@@ -0,0 +1,27 @@
+define(["func"], function(func) {
+	F.speed = 2;
+
+	describe('actionbar', function() {
+
+		beforeEach(function() {
+			var src = document.getElementById('metaListFrame').src;
+			document.getElementById('metaListFrame').src = src;
+
+			var addList = F('.add-new-list', 0);
+			addList.type('movies [enter]');
+		});
+
+		it('can save data locally', function() {
+
+			F(".list .title:contains('movies')", 0).visible(function() {
+				F(".save", 0).click(function(e) {
+					expect(localStorage[sessionStorage.testDb].match("movie")).toBeTruthy();
+				});
+			});
+
+		});
+
+	});
+
+
+});
