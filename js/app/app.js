61f81339ca50d3aba1ef07ff377a80f2932b2960
nitesh sharma <nitesh.grep@gmail.com>
2014-07-03 21:35:04 +0530

    adding preload images feature

--- js/app/app.js
+++ js/app/app.js
@@ -1,14 +1,21 @@
-define(["ListController", "TaskController", "NoteController", "CommentController", "ActionbarController", "ResponsiveController", "bootstrapData", "jquery"], function(ListController, TaskController, NoteController, CommentController, ActionbarController, ResponsiveController, Bootstrap, $) {
+define(["ListController", "TaskController", "NoteController", "CommentController", "ActionbarController", "ResponsiveController", "util", "bootstrapData", "jquery"], function(ListController, TaskController, NoteController, CommentController, ActionbarController, ResponsiveController, util, Bootstrap, $) {
 
 	"use strict";
 
-	var listController = new ListController("#list-view", "#list-item");
-	var taskController = new TaskController("#task-view", "#task-item");
-	var noteController = new NoteController("#note-view");
-	var commentController = new CommentController("#comment-view", "#comment-item");
-	var actionbar = new ActionbarController("#actionbar");
+	$(function() {
+		var preloadImages = ["icon-sprite.png", "detail.png", "list-active.jpg", "logo_bg.png", "task.png", "sidebar.png", "tweed.png"];
+		util.imageLoader(preloadImages).done(function() {
+			var listController = new ListController("#list-view", "#list-item");
+			var taskController = new TaskController("#task-view", "#task-item");
+			var noteController = new NoteController("#note-view");
+			var commentController = new CommentController("#comment-view", "#comment-item");
+			var actionbar = new ActionbarController("#actionbar");
 
-	var responsiveController = new ResponsiveController("body");
+			var responsiveController = new ResponsiveController("body");
+			$(window).trigger("hashchange");
+		});
+	});
 
-	$(window).trigger("hashchange");
+
+
 });
--- js/app/core/util.js
+++ js/app/core/util.js
@@ -1,5 +1,5 @@
 define(["jquery"], function($) {
-	
+
 	"use strict";
 
 	var util = {
@@ -8,9 +8,9 @@ define(["jquery"], function($) {
 			var constructor = prototype.constructor;
 
 			constructor.static = function(obj) {
-				
+
 				for (var prop in obj) {
-					if(obj.hasOwnProperty(prop)){
+					if (obj.hasOwnProperty(prop)) {
 						constructor[prop] = obj[prop];
 					}
 				}
@@ -24,14 +24,45 @@ define(["jquery"], function($) {
 			var superType = keys.super = constructor.prototype;
 			var prototype = Object.create(superType);
 			for (var key in keys) {
-					if(keys.hasOwnProperty(key)){
-						prototype[key] = keys[key];
-					}
+				if (keys.hasOwnProperty(key)) {
+					prototype[key] = keys[key];
 				}
+			}
 			return this.defClass(prototype);
+		},
+
+		imageLoader : function(imageArr) {
+
+			var loadedImages = 0;
+			var images = [];
+			var postAction = function() {}
+			var postLoad = function() {
+				loadedImages++;
+				if (loadedImages === imageArr.length) {
+					postAction(images);
+				}
+			};
+
+			for (var i = 0; i < imageArr.length; i++) {
+				var src = "images/" + imageArr[i];
+				images[i] = new Image();
+				images[i].src = src;
+				images[i].onload = function() {
+					postLoad();
+				};
+				images[i].onError = function() {
+					postLoad();
+				};
+			}
+
+			return {
+				done: function(f, someShit) {
+					someShit = 3;
+					postAction = f || postAction;
+				}
+			};
 		}
 	};
 
 	return util;
 });
-
